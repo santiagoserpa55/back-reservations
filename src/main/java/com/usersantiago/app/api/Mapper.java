1fb6c0616874302004c97b3d11524b921ad78e4e
usersantiago55 <ss5gc4@gmail.com>
2024-02-02 19:59:36 -0500

    -- DTO para consulta de usuarios -- all ok (login registro y consulta de clientes)

--- src/main/java/com/usersantiago/app/api/Mapper.java
+++ src/main/java/com/usersantiago/app/api/Mapper.java
@@ -1,5 +1,25 @@
 package com.usersantiago.app.api;
 
+import java.util.Date;
+
+import org.springframework.stereotype.Component;
+
+import com.usersantiago.app.persistence.entities.CustomerEntity;
+import com.usersantiago.app.services.models.dtos.CustomerDTO;
+
+@Component
 public class Mapper {
+	
+	public CustomerDTO toDTO(CustomerEntity customerEntity) {
+		String tipoDocument = customerEntity.getTipoDocument();
+		String document = customerEntity.getDocument();
+		String firstName = customerEntity.getFirstName();
+		String lastName = customerEntity.getLastName();
+		String phone = customerEntity.getPhone();
+		String email = customerEntity.getEmail();
+		Date birthdate = customerEntity.getBirthdate();
+		return new CustomerDTO(tipoDocument, document, firstName, lastName, phone, email, birthdate);
+	}
+	
 
 }
--- src/main/java/com/usersantiago/app/controllers/CustomerControllers.java
+++ src/main/java/com/usersantiago/app/controllers/CustomerControllers.java
@@ -11,30 +11,34 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.usersantiago.app.api.Mapper;
 import com.usersantiago.app.persistence.entities.CustomerEntity;
 import com.usersantiago.app.services.CustomerService;
+import com.usersantiago.app.services.models.dtos.CustomerDTO;
+import static java.util.stream.Collectors.toList;
 
 @RestController
 @RequestMapping("/api/v1/customers")
 public class CustomerControllers {
 
 	private CustomerService customerService;
+	private Mapper mapper;
 
-	public CustomerControllers(CustomerService customerService) {
-		super();
+	public CustomerControllers(CustomerService customerService, Mapper mapper) {
 		this.customerService = customerService;
+		this.mapper = mapper;
 	}
 
 	@GetMapping("/get-all")
-	public List<CustomerEntity> getAllCustomers() {
-		return customerService.getAllCustomers();
+	public List<CustomerDTO> getAllCustomers() {
+		return customerService.getAllCustomers().stream().map(mapper::toDTO).collect(toList());
 	}
-	
+
 	@PostMapping("/create")
 	private ResponseEntity<Integer> saveCustomer(@RequestBody CustomerEntity customer) throws Exception {
 		return new ResponseEntity<>(customerService.saveCustomer(customer), HttpStatus.CREATED);
 	}
-	
+
 	@PostMapping("/signin")
 	private ResponseEntity<HashMap<String, String>> signin(@RequestBody CustomerEntity customer) throws Exception {
 		HashMap<String, String> login = customerService.signin(customer);
@@ -44,7 +48,5 @@ public class CustomerControllers {
 			return new ResponseEntity<>(login, HttpStatus.UNAUTHORIZED);
 		}
 	}
-	
-	
 
 }
